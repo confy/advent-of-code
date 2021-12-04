8777bb02b9a47a7823c57980c91f59ddd614c85f
Adrian McFarlane <confuzedfx@gmail.com>
2021-12-04 00:28:38 -0800

    🛐

--- 04/4.py
+++ 04/4.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 def read_input(filename):
     f = open(filename, 'r').read().rstrip()
     calls, *boards = f.split('\n\n')
@@ -9,16 +10,18 @@ def read_input(filename):
     boards = [b.split() for b in boards]
     final_boards = []
     for board in boards:
-        final_board =[]
+        final_board = []
         for i in range(5):
             line = [int(num) for num in board[i*5:i*5+5]]
             final_board.append(line)
         final_boards.append(final_board)
-        
+
     return calls, final_boards
+
 
 def check_row(row, called_so_far):
     return all(num in called_so_far for num in row)
+
 
 def check_board(board, called_so_far):
     board_cols = list(map(list, itertools.zip_longest(*board, fillvalue=None)))
@@ -29,7 +32,6 @@ def check_board(board, called_so_far):
         if check_row(col, called_so_far):
             return True, board
     return False, board
-    
 
 
 if __name__ == '__main__':
@@ -38,7 +40,7 @@ if __name__ == '__main__':
     calls = calls[5:]
     winners = []
     called_at_last_win = []
-    
+
     for call in calls:
         for i, board in enumerate(boards):
             if board == []:
@@ -51,15 +53,11 @@ if __name__ == '__main__':
 
         if any(board != [] for board in boards):
             called_so_far.append(call)
-            
+
     vals_to_sum = []
     for row in winners[-1]:
         for val in row:
             if val not in called_at_last_win:
                 vals_to_sum.append(val)
-                
+
     print(sum(vals_to_sum)*called_at_last_win[-1])
-        
-            
-            
-            
