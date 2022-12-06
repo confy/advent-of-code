fc39826e8434b655d7888f9481cb5fee52188ef4
Adrian McFarlane <confuzedfx@gmail.com>
2022-12-05 21:41:27 -0800

    🌟

---
+++ 2022/06/6.py
@@ -0,0 +1,16 @@
+import collections
+
+
+def check_cipher(text, n):
+    last_n = collections.deque(maxlen=n)
+    for i, char in enumerate(text):
+        last_n.append(char)
+        if all(last_n.count(c) <= 1 for c in last_n) and len(last_n) == n:
+            return i+1
+
+
+with open('./2022/06/input.txt') as f:
+    text = f.read()
+
+print(check_cipher(text, 4),  # part 1
+      check_cipher(text, 14))  # part 2
