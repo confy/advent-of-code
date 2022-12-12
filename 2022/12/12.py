e25a077f62a367fed195cccd898b12b3092d9806
Adrian McFarlane <confuzedfx@gmail.com>
2022-12-11 23:02:43 -0800

    👪

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -1,36 +1,39 @@
-from operator import itemgetter
+from collections import deque
 with open("./2022/12/input.txt") as f:
     data = f.read().splitlines()
 
 grid = {}
-queue = []
+queue = deque()
 for y, line in enumerate(data):
     for x, char in enumerate(line):
         if char == "S":
             grid[(x, y)] = ["a", None]
-            #queue.append([(x, y), 0]) # part 1
+            # queue.append([(x, y), 0]) # part 1
         elif char == "E":
             grid[(x, y)] = ["z", None]
-            #target = (x,y) # part 1
-            queue.append([(x, y), 0]) # part 2
+            # target = (x,y) # part 1
+            queue.append([(x, y), 0])  # part 2
         else:
             grid[(x, y)] = [char, None]
 
-grid[queue[0][0]][1] = 0 #steps to reach cell
+grid[queue[0][0]][1] = 0  # steps to reach cell
+
 
 def near(pos):
     return {(pos[0] + 1, pos[1]), (pos[0], pos[1] + 1), (pos[0] - 1, pos[1]), (pos[0], pos[1] - 1)}
 
-def search():
+
+def search(grid, queue):
     while True:
-        queue.sort(key=itemgetter(1))
-        cell = queue.pop(0)
+        cell = queue.popleft()
         for i in near(cell[0]):
-            # if i in grid and grid[i][1] == None and ord(grid[i][0])-1 <= ord(grid[cell[0]][0]): # part 1
-            #     if i == target: return cell[1]+1
+          # if i in grid and grid[i][1] == None and ord(grid[i][0])-1 <= ord(grid[cell[0]][0]): # part 1
+            #   if i == target: return cell[1]+1
             if i in grid and grid[i][1] is None and ord(grid[i][0]) >= ord(grid[cell[0]][0]) - 1:
-                if grid[i][0] == "a": return cell[1]+1
+                if grid[i][0] == "a":
+                    return cell[1]+1
                 grid[i][1] = cell[1]+1
                 queue.append([i, cell[1]+1])
 
-print(search())
+
+print(search(grid, queue))
--- 2022/12/12mini.py
+++ 2022/12/12mini.py
@@ -1,16 +1,14 @@
-from operator import itemgetter
-with open('./2022/12/input.txt')as f:data=f.read().splitlines()
-grid={}
-queue=[]
+from collections import deque
+data=open('./2022/12/input.txt').read().splitlines();grid,queue={},deque()
 for (y,line) in enumerate(data):
-	for (x,char) in enumerate(line):
-		if char=='S':grid[(x,y)]=['a',None]
-		elif char=='E':grid[(x,y)]=['z',None];queue.append([(x,y),0])
-		else:grid[(x,y)]=[char,None]
+    for (x,char) in enumerate(line):
+        if char=='S':grid[(x,y)]=['a',None]
+        elif char=='E':grid[(x,y)]=['z',None];queue.append([(x,y),0])
+        else:grid[(x,y)]=[char,None]
 grid[queue[0][0]][1]=0
 while True:
-	queue.sort(key=itemgetter(1));cell=queue.pop(0)
-	for i in {(cell[0][0]+1,cell[0][1]),(cell[0][0],cell[0][1]+1),(cell[0][0]-1,cell[0][1]),(cell[0][0],cell[0][1]-1)}:
-		if i in grid and grid[i][1]is None and ord(grid[i][0])>=ord(grid[cell[0]][0])-1:
-			if grid[i][0]=='a':print(cell[1]+1);exit()
-			grid[i][1]=cell[1]+1;queue.append([i,cell[1]+1])
+    cell=queue.popleft()
+    for i in [(cell[0][0]+1,cell[0][1]),(cell[0][0],cell[0][1]+1),(cell[0][0]-1,cell[0][1]),(cell[0][0],cell[0][1]-1)]:
+        if i in grid and grid[i][1]is None and ord(grid[i][0])>=ord(grid[cell[0]][0])-1:
+            if grid[i][0]=='a':print(cell[1]+1);exit()
+            grid[i][1]=cell[1]+1;queue.append([i,cell[1]+1])
