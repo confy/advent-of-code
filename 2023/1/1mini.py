794829f25c61120fdfc6ee886557d766bbaa7cfc
Adrian McFarlane <confuzedfx@gmail.com>
2023-12-01 12:49:11 -0800

    ☃️

--- 2023/1/1mini.py
+++ 2023/1/1mini.py
@@ -1,14 +1,15 @@
-with open('input.txt')as f:lines=f.readlines()
-def solve(lines):
-	A=None;vals=[]
-	for line in lines:
-		first,last=A,A
-		for char in line:
-			if char.isnumeric():first=char if first is A else first;last=char
-		vals.append(int(first+last))
-	return sum(vals)
-print(solve(lines))
-pairs=[('one','o1e'),('two','t2o'),('three','t3e'),('four','f4r'),('five','f5e'),('six','s6x'),('seven','s7n'),('eight','e8t'),('nine','n9e')]
-new_lines=[]
-for line in lines:[(line:=line.replace(w,d))for(w,d)in pairs];new_lines.append([c for c in line if c.isnumeric()])
-print(solve(new_lines))
+D=print
+with open('input.txt')as E:A=E.readlines()
+def B(lines):
+	C=None;D=[]
+	for F in lines:
+		A,E=C,C
+		for B in F:
+			if B.isnumeric():A=B if A is C else A;E=B
+		D.append(int(A+E))
+	return sum(D)
+D(B(A))
+F=[('one','o1e'),('two','t2o'),('three','t3e'),('four','f4r'),('five','f5e'),('six','s6x'),('seven','s7n'),('eight','e8t'),('nine','n9e')]
+C=[]
+for G in A:[(A:=A.replace(B,C))for(B,C)in F];C.append([A for A in G if A.isnumeric()])
+D(B(C))
