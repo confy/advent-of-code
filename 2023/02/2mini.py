c2951635dfc0fa3c8d0216245a08a53d68766ef3
Adrian McFarlane <confuzedfx@gmail.com>
2023-12-09 23:31:34 -0800

    🏌️

--- 2023/02/2mini.py
+++ 2023/02/2mini.py
@@ -1,9 +1,9 @@
-# lol
+# lol 🏌️ ⛳
 import re
-with open('input.txt')as H:I=[A.strip()for A in H.readlines()]
+I=open('input.txt').read().splitlines()
 def A(l,c):return max(int(A.group(1))for A in re.finditer('(\\d+) '+c,l))
-C,D=[],[]
-for(J,B)in enumerate(I):
-	E=A(B,'red');F=A(B,'green');G=A(B,'blue');D.append(E*F*G)
-	if not(E>12 or F>13 or G>14):C.append(J+1)
-print(sum(C),sum(D))
+C,D=0,0
+for(J,B)in enumerate(I,1):
+	E=A(B,'red');F=A(B,'green');G=A(B,'blue');D+=E*F*G
+	if not(E>12 or F>13 or G>14):C+=J
+print(C,D)
